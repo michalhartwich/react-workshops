ef6f76c39f8825cd88598127991887e5eb1dffdc
Sebastian Brych <sebastian+kraken@nwscx.com>
2016-09-23 11:21:35 +0200

    Link state of form in PostForm component

--- app/react/components/posts/PostForm.js
+++ app/react/components/posts/PostForm.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
+import linkState from 'react-link-state';
 
 class PostForm extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      content: '',
+      author: ''
+    }
+  }
   render() {
     return (
       <form>
         <div className="form-group">
           <label for="title">Title</label>
-          <input id="title" className="form-control" type="" placeholder="Title" />
+          <input id="title" valueLink={linkState(this, 'title')} className="form-control" type="text" placeholder="Title" />
         </div>
         <div className="form-group">
           <label for="content">Content</label>
-          <textarea id="content" className="form-control" rows="3" placeholder="Content" />
+          <textarea id="content" valueLink={linkState(this, 'content')} className="form-control" rows="3" placeholder="Content" />
         </div>
         <div className="form-group">
           <label for="author">Author</label>
-          <input id="author" className="form-control" type="" placeholder="Author" />
+          <input id="author" valueLink={linkState(this, 'author')} className="form-control" type="text" placeholder="Author" />
         </div>
         <button className="btn btn-default" type="submit">Submit</button>
       </form>
