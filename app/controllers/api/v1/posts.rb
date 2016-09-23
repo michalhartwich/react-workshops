916af2cdfc77253a921aa80ff0a3c9369aae01f9
Michał Hartwich <michal.hartwich@netguru.pl>
2016-09-23 10:29:48 +0200

    post setup

---
+++ app/controllers/api/v1/posts.rb
@@ -0,0 +1,78 @@
+module API
+  module V1
+    class Posts < Grape::API
+      version 'v1'
+      format :json
+
+      resource :posts do
+        desc 'Show post'
+        params do
+          requires :id, type: Integer, desc: 'Post id'
+        end
+        route_param :id do
+          get do
+            post = Post.find_by(id: params[:id])
+            if post
+              post
+            else
+              status :not_found
+            end
+          end
+        end
+
+        desc 'Deletes post'
+        params do
+          requires :id, type: Integer, desc: 'Post id'
+        end
+        route_param :id do
+          delete do
+            post = Post.find_by(id: params[:id])
+            return status :not_found if post.nil?
+            if post.destroy
+              post
+            else
+              status :unprocessable_entity
+            end
+          end
+        end
+
+        desc 'Add post'
+        params do
+          requires :email, type: String, desc: 'E-mail address'
+          requires :password, type: String, desc: 'Password'
+        end
+        post do
+          params[:password_confirmation] = params[:password] # YOLO XD
+          post = Post.new(params)
+          if post.save
+            post
+          else
+            status :unprocessable_entity
+          end
+        end
+
+        desc 'Updates post'
+        params do
+          requires :id, type: Integer, desc: 'Post id'
+          requires :email, type: String, desc: 'E-mail address'
+        end
+        route_param :id do
+          put do
+            post = Post.find_by(id: params[:id])
+            return status :not_found if post.nil?
+            if post.update_attributes(email: params[:email])
+              post
+            else
+              status :unprocessable_entity
+            end
+          end
+        end
+
+        desc 'Get all posts'
+        get do
+          Post.all
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   mount API::V1::Users => '/api'
+  mount API::V1::Posts => '/api'
 
   get '/' => 'app#index'
   get '/admin' => 'app#admin'
   get '/counter' => 'app#counter'
+
+  resources :posts
 end
