a04e0a80d0b59fe55f928e265dd9e2bfae930a6f
Krzysztof Kołek <krzysztof.kolek@netguru.pl>
2016-09-23 11:18:29 +0200

    Add post element styling

--- app/react/components/posts/post-list-element.js
+++ app/react/components/posts/post-list-element.js
@@ -3,15 +3,11 @@ import React from 'react';
 export default class PostListElement extends React.Component {
   render() {
     return (
-      <div>
-        <row>
-          <div className="col-md-6">
-            {this.props.author}
-          </div>
-          <div className="col-md-6">
-            {this.props.title}
-          </div>
-        </row>
+      <div className="panel panel-default">
+        <div className="panel-heading">{this.props.author}</div>
+        <div className="panel-body">
+          {this.props.title}
+        </div>
       </div>
     );
   }
